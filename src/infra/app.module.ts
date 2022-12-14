f631a20a27e3af5a7eb0ac0a7b6ea4a2f051a25a
Danilo Gonçalves <danilo.santos@grupoboticario.com.br>
2022-12-13 21:59:44 -0300

    refactor: infrastructure layer restructuring

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
+import { HttpModule } from './http/http.module';
 
 @Module({
-  imports: [],
+  imports: [HttpModule, DatabaseModule],
 })
 export class AppModule {}
