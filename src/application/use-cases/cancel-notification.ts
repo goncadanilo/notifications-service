6e67b7790e13cdaeaaf6966a6f443d53ea32a2e7
Danilo Gonçalves <danilo.santos@grupoboticario.com.br>
2022-12-15 18:19:42 -0300

    feat: create use case to cancel a notification

---
+++ src/application/use-cases/cancel-notification.ts
@@ -0,0 +1,27 @@
+import { NotificationsRepository } from '@application/repositories/notifications-repository';
+import { Injectable } from '@nestjs/common';
+import { NotificationNotFound } from './errors/notification-not-found';
+
+interface CancelNotificationRequest {
+  notificationId: string;
+}
+
+@Injectable()
+export class CancelNotification {
+  constructor(private notificationRepository: NotificationsRepository) {}
+
+  async execute(request: CancelNotificationRequest): Promise<void> {
+    const { notificationId } = request;
+    const notification = await this.notificationRepository.findById(
+      notificationId,
+    );
+
+    if (!notification) {
+      throw new NotificationNotFound();
+    }
+
+    notification.cancel();
+
+    await this.notificationRepository.save(notification);
+  }
+}
